b8637a1fb22bab40a0eb080e55bb51c129b59fd1
Trevor <Trevor@Trevors-laptop>
2016-11-03 14:29:06 +0000

    First Commit of my project

---
+++ src/main/java/com/caveofprogramming/App.java
@@ -0,0 +1,23 @@
+package com.caveofprogramming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@EnableAutoConfiguration
+@Controller
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+	
+	@ResponseBody
+	@RequestMapping("/")
+	String home(){
+		return "Hello World";
+	}
+
+}
